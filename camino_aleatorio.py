fc955d18e014908c600cc346f3604c7f43f6bc9b
Martin Cruz <martin_crdev@outloock.com>
2021-09-25 18:55:14 -0500

    Agregado programa estadisticas y probabilidades

--- camino_aleatorio.py
+++ camino_aleatorio.py
@@ -1,7 +1,14 @@
 from borracho import BorrachoTradicional
 from campo import Campo
 from coordenada import Coordenada
+from bokeh.plotting import figure, show
 
+
+def graficar(x, y):
+    grafica = figure(title="Camino aleatorio", x_axis_label="pasos", y_axis_label="distacia recorrida")
+    grafica.line(x, y, legend_label="distancia media")
+
+    show(grafica)
 
 def caminata(campo, borracho, pasos):
     inicio = campo.obtener_coordenada(borracho)
@@ -25,16 +32,19 @@ def simular_caminata(pasos, numero_de_intentos, tipo_de_borracho):
     return distancias
 
 def run(distancias_de_caminata, numero_de_intentos, tipo_de_borracho):
+    distanias_medias_por_caminata = []
     
     for pasos in distancias_de_caminata:
         distancias = simular_caminata(pasos, numero_de_intentos, tipo_de_borracho)
         distancia_media = round(sum(distancias) / len(distancias), 4)
         distancia_maxima = max(distancias)
         distancia_minima = min(distancias)
+        distanias_medias_por_caminata.append(distancia_media)
         print(f'{tipo_de_borracho.__name__} caminata aleatoria de {pasos} pasos')
         print(f'Media = {distancia_media}')
         print(f'Max = {distancia_maxima}')
         print(f'Min = {distancia_minima}')
+    graficar(distancias_de_caminata, distanias_medias_por_caminata)
 
 
 if __name__ == "__main__":
